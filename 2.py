b6978a08db2243795e940530bc3a32c45479432c
Michail Koryukin <kom1969@list.ru>
2022-11-09 11:05:23 +0500

    add working version without classes

---
+++ 2.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+           'квадратного корня из заданного числа')
+
+
+def calculatesquareroot(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
+
+
+def calc(your_number):
+    """Получим результат."""
+    if your_number > 0:
+        root = calculatesquareroot(your_number)
+        print('Мы вычислили квадратный корень '
+              'из введённого вами числа. '
+              'Это будет:', root)
+
+
+print(message)
+
+calc(25.5)
